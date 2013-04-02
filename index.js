1bf997e645a720aa8f450955c728c1f1fbab12eb
Tom Gallacher <tomgallacher23@gmail.com>
2013-04-02 17:38:07 +0100

    First working version

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 exports.StoreStream = require('./lib/store-stream')
 exports.CollectionStream = require('./lib/collection-stream')
+exports.CryptoStream = require('./lib/crypto-stream')
+exports.RetrieveStream = require('./lib/retrieve-stream')
